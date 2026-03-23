a0489c8141d4c874ef2a59dab94a21063d36cb0e
Zeteny Levay <levay.zeteny3@gmail.com>
2026-03-23 12:51:40 +0100

    created ui.cpp and ui.h

---
+++ src/ui/ui.cpp
@@ -0,0 +1,5 @@
+//
+// Created by levay on 3/23/2026.
+//
+
+#include "ui.h"
---
+++ src/ui/ui.h
@@ -0,0 +1,8 @@
+//
+// Created by levay on 3/23/2026.
+//
+
+#ifndef PROJECT_2_2_GROUP_1_UI_H
+#define PROJECT_2_2_GROUP_1_UI_H
+
+#endif //PROJECT_2_2_GROUP_1_UI_H
