a6a3f531cd3f9be27ff9c45d2c7cc36a08e1de4f
Berke Sevahil <42012824+mygizli04@users.noreply.github.com>
2026-03-26 14:35:38 +0100

    Increase OpenGL version, add build files to .gitignore

--- src/ui/ui.cpp
+++ src/ui/ui.cpp
@@ -19,7 +19,7 @@ void startGui() {
     ImGui::CreateContext();
     
     ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 150");
+    ImGui_ImplOpenGL3_Init("#version 330");
 
     // The main loop
     while(!glfwWindowShouldClose(window)) {
