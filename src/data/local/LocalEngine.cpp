731b35c2e11b67eb5351002647e4c4facadc1e4c
FMGY0nko <theevilrabbit999@gmail.com>
2026-05-11 21:40:32 +0200

    fix for buoyancy

--- src/data/local/LocalEngine.cpp
+++ src/data/local/LocalEngine.cpp
@@ -217,7 +217,7 @@ void LocalEngine::Collision(double tauT,double TempAvg,double tauF, Grid& gridNe
             }
             //buoyancy is calculated using a simplied version of the Boussinesq approximation: beta * (T-Tavg)
             //buoyancy represents how much the hot fluid wants to rise up
-            double buoyancy = -lattice_buoyancy *(temp-ROOM_TEMP);  //4*1e-5 represents the thermal expansion strenght
+            double buoyancy = lattice_buoyancy * (temp-ROOM_TEMP);  //4*1e-5 represents the thermal expansion strenght
 
             //we use half force to better represent how and when the force is applied, the second half will be added from the forceTerm
             // because the buoyancy value of ux is 0 we do not need to calculate the half force term of ux, we can just use ux
